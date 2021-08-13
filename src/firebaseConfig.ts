b171945c4628f66dc2e953dc9925bad6d9262aea
TrafalMar <vladiksav2@gmail.com>
2021-08-13 18:54:26 +0300

    added env

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const { REACT_APP_API_KEY } = process.env;
+
+export const firebaseConfig = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain: "uchat-e9e68.firebaseapp.com",
+  projectId: "uchat-e9e68",
+  storageBucket: "uchat-e9e68.appspot.com",
+  messagingSenderId: "415955918693",
+  appId: "1:415955918693:web:5782e11d1cfb4589d80b8c",
+  measurementId: "G-XDNB54N581",
+};
